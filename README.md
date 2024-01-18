199aa538e0797a24eeae2f7f5fc4fb888d7e5a3f
Ruberald <sajidhossain@gmail.com>
2024-01-18 16:08:23 +0530

    Basic design made

---
+++ README.md
@@ -0,0 +1,27 @@
+# gfg-website
+
+The source code for the official website of Geeks For Geeks BVCOE chapter.
+
+## Project Setup
+
+```sh
+yarn
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+yarn dev
+```
+
+### Compile and Minify for Production
+
+```sh
+yarn build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+yarn run lint
+```
---
+++ src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Team from '../components/Team.vue'
+</script>
+
+<template>
+  <main>
+    <div class="about px-4 container mx-auto">
+      <div class="my-8 flex items-center justify-between md:justify-center">
+        <span class="border-b-2 border-t-2 w-8 md:w-24 inline-block mr-2 rounded"></span>
+
+        <h1 class="text-gray-800 md:text-2xl font-bold text-center">
+          About us
+        </h1>
+
+        <span class="border-b-2 border-t-2 w-8 md:w-24 inline-block ml-2 rounded"></span>
+      </div>
+      <h1>We're an active, inspired bunch of youth etching our steps into the sand of the world at every turn! Come join us on this journey of a technically thrilling ecstasy!</h1>
+    </div>
+    <Team />
+  </main>
+</template>
+
+<style>
+/*  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+*/</style>
